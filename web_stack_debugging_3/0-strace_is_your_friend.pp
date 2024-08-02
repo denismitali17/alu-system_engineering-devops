f2871ae71ffd1e84090c13adf8c8a8b5eb94b3d1
denismitali17 <d.mitali1@alustudent.com>
2024-08-02 05:21:58 -0700

    Strace is your friend

---
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# trace your friends
+d "phpp" extensions to "php" in "wp-settings.php".
+
+exec{'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
