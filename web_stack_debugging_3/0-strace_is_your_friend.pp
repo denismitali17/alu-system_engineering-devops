4e0bc89201003f5031f903f87022baa4d343650f
denismitali17 <d.mitali1@alustudent.com>
2024-08-06 11:38:01 -0700

    Strace is your friend

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,6 @@
-# trace your friends
-d "phpp" extensions to "php" in "wp-settings.php".
-
-exec{'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+# Fix Appache 500, automate with puppet
+exec { 'fixing typo...':
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
